71bce4389fefd49855eeff32784aa7cc2f3f8b14
Raphael Baron <raphaelbaron@gmail.com>
2012-11-20 22:07:41 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,125 @@
+remotebox
+=========
+
+Remotebox is a plugin for rhythmbox [Rhythmbox](http://projects.gnome.org/rhythmbox/) which opens a socket at port 36666 and listens for remote commands. It was developed as a backend for [Remotebox](http://not.online.yet/) android app, but it can be used by any application that supports communication via TCP sockets.
+
+Installation
+------------
+
+To install the plugin, simply copy the files `rhythmbox.py` and `rhythmbox.plugin` to your Rhythmbox plugin directory, usually `~/.local/share/rhythmbox/plugins/`. If you want to automate this process, simply type
+
+```
+ $ 
+```
+
+on your terminal.
+
+Usage
+-----
+
+As said above, you can use remotebox to control your rhythmbox remotely through a TCP socket. Once the plugin is installed and Rhythmbox is running, a typical remote session might be as follows (using netcat as a client, assuming Rhythmbox us running and playing a file on IP 192.168.0.100):
+
+```shell
+ $ netcat 192.168.0.100
+
+next # -> Client request
+ok # -> remotebox response
+
+prev
+ok
+
+vol
+1.0
+
+vol 0.3
+ok
+
+vol
+0.3
+
+pause 
+ok
+
+goto file://path/to/a/file.mp3
+ok
+
+stop
+ok
+
+list # -> Request the list of all files on Rhythmbox library
+... # -> Response is a xml-formatted string. See "Avaliable commands" for more information.
+```
+
+Available commands
+------------------
+
+<table>
+<td>Request</td><td>Response</td><td>Explanation</td>
+<td>play</td><td>`ok\n`</td><td>If there isn't any file selected, the response is ``</td>
+<td>pause</td><td>`ok\n`</td><td></td>
+<td>stop</td><td>`ok\n`</td><td></td>
+<td>next</td><td>`ok\n`</td><td></td>
+<td>prev</td><td>`ok\n`</td><td></td>
+<td>vol</td><td>`0.9\n`</td><td>The response is the current volume setting, a float point between 0.0 and 1.0</td>
+<td>vol 0.7</td><td>`ok\n`</td><td>Set the volume to a float point between 0.0 and 1.0. If the requested setting is invalid, return ``</td>
+<td>goto file://path/to/a/file.mp3</td><td>`ok\n`</td><td>Plays the specified file. If it doens't exist, return ``</td>
+<td>list</td><td>xml-formatted string containing base64 encoded information about the files on rhythmbox library.</td><td>See example below</td>
+
+#Example of a xml-formatted file list in response to a `list` request#
+
+```
+<xml version='1.0' encoding='utf-8'>
+  <tracks>
+    <track>
+      <artist>VGhlIEJlYXRsZXMK</artist>
+      <title>V2l0aCBhIGxpdHRsZSBoZWxwIGZyb20gbXkgZnJpZW5kcwo=</title>
+      <album>U2d0LiBQZXBwZXLigJlzIExvbmVseSBIZWFydHMgQ2x1YiBCYW5kCg==</album>
+      <url>ZmlsZTovL215L2F1ZGlvL2ZvbGRlci90aGUlMjBiZWF0bGVzJTIwLSUyMHdpdGglMjBhJTIwbGl0dGxlJTIwaGVscCUyMGZyb20lMjBteSUyMGZyaWVuZHMubXAzCg==</url>
+    </track>
+    <track>
+      <artist>VGhlIEJlYXRsZXMK</artist>
+      <title>THVjeSBpbiB0aGUgZGt5IHdpdGggZGlhbW9uZHMK</title>
+      <album>U2d0LiBQZXBwZXLigJlzIExvbmVseSBIZWFydHMgQ2x1YiBCYW5kCg==</album>
+      <url>ZmlsZTovL215L2F1ZGlvL2ZvbGRlci90aGUlMjBiZWF0bGVzJTIwLSUyMGx1Y3klMjBpbiUyMHRoZSUyMHNreSUyMHdpdGglMjBkaWFtb25kcy5tcDMK</url>
+    </track>
+    .
+    .
+    .
+  </tracks>
+</xml>
+
+```
+The values in everyfield (<artist>, <title>, <album>, <url>) are base64 encoded uft8 strings. If we decode these fields, we get:
+
+```
+<xml version='1.0' encoding='utf-8'>
+  <tracks>
+    <track>
+      <artist>The Beatles</artist>
+      <title>With a little help from my friends</title>
+      <album>Sgt. Pepper’s Lonely Hearts Club Band</album>
+      <url>file://my/audio/folder/the%20beatles%20-%20with%20a%20little%20help%20from%20my%20friends.mp3</url>
+    </track>
+    <track>
+      <artist>The Beatles</artist>
+      <title>Lucy in the dky with diamonds</title>
+      <album>Sgt. Pepper’s Lonely Hearts Club Band</album>
+      <url>file://my/audio/folder/the%20beatles%20-%20lucy%20in%20the%20sky%20with%20diamonds.mp3</url>
+    </track>
+    .
+    .
+    .
+  </tracks>
+</xml>
+
+```
+
+Contribute
+----------
+
+Please feel free to add suggestions. If you have written a client which is compatible with this plugin, tell me and I'll mention it here! :)
+
+License
+-------
+
+[MIT](http://opensource.org/licenses/MIT).
---
+++ remotebox.py
@@ -0,0 +1,247 @@
+from gi.repository import GObject, RB, Peas
+
+import threading
+import socket
+import time
+import string
+import re
+import base64
+
+# Empty HOST - INADDR_ANY
+HOST = ''
+PORT = 30666
+BUFFSIZE = 4096
+
+class RemoteboxPlugin(GObject.Object, Peas.Activatable):
+    object = GObject.property(type=GObject.Object)
+
+    def __init__(self):
+        super(RemoteboxPlugin, self).__init__()
+
+    def do_activate(self):
+        print "Hello Remotebox!"
+        shell = self.object
+
+        # Start a new thread, pass self.object = self.shell as argument
+        t = MyThread(self.object)
+        t.daemon = True
+        t.start()
+
+    def do_deactivate(self):
+        print "Goodbye Remotebox!"
+
+class SockServer:
+  def __init__(self):
+    self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    self.addr = (HOST, PORT)
+    self.sock.bind(self.addr)
+    self.sock.listen(1)
+
+  #Blocks here until someone connects
+  def accept(self):
+    (self.clientSock, self.clientAddr) = self.sock.accept()
+    print "Client connected!"
+
+  def receive(self):
+    self.buff = self.clientSock.recv(BUFFSIZE)
+    return self.buff
+
+  def send(self, text):
+    self.clientSock.send(text)
+
+  def cleanup(self):
+    self.clientSock.close()
+
+class MyThread(threading.Thread):
+  def __init__(self, RBShell):
+    super(MyThread, self).__init__()
+    self.srv = SockServer()
+    self.shell = RBShell
+
+  def run(self):
+    while(1):
+      print "Accepting..."
+      self.srv.accept()
+      while(1):
+        print "Receiving..."
+        try:
+          self.buff = self.srv.receive()
+    
+          if len(self.buff) == 0: 
+            print "Received 0 bytes. Assuming disconnection."
+            break
+  
+          print "Received: ",self.buff
+
+          parts = string.split(self.buff)
+
+          # Try to run rhythmbox api functions
+          try:
+
+            if parts[0] == "play":
+              try:
+                self._play()
+                self.srv.send("ok\n")
+              except Exception, e:
+                self.srv.send("Error on play: no track selected!\n")
+
+            elif parts[0] == "pause":
+              try:
+                self._pause()
+                self.srv.send("ok\n")
+              except Exception, e:
+                self.srv.send("Error on pause: no track selected!\n")
+
+            elif parts[0] == "stop":
+              try:
+                self._stop()
+                self.srv.send("ok\n")
+              except Exception, e:
+                self.srv.send("Error on stop: no track selected!\n")
+
+            elif parts[0] == "next":
+              try:
+                self._next()
+                self.srv.send("ok\n")
+              except Exception, e:
+                self.srv.send("Error on next: no track selected!\n")
+
+            elif parts[0] == "prev":
+              try:
+                self._prev()
+                self.srv.send("ok\n")
+              except Exception, e:
+                self.srv.send("Error on prev: no track selected!\n")
+
+            elif parts[0] == "goto":
+                self._goto(parts[1])
+                self.srv.send("ok\n")
+
+            elif parts[0] == "vol":
+                if len(parts) == 1:
+                  print "Volume: "+str(self._getVol())
+                  #self.srv.send("ok\n")
+                  self.srv.send(str(self._getVol())+"\n")
+
+                else:
+                  # Try to convert to float
+                  try:
+                    if float(parts[1]) <= 1 and float(parts[1]) >= 0:
+                      print "Set volume to "+str(float(parts[1]))
+                      self._setVol(float(parts[1]))
+                      self.srv.send("ok\n")
+                    else:
+                      print "Invalid volume setting."
+                      self.srv.send("Invalid volume setting.\n")
+                  except Exception, e:
+                    print "Invalid volume setting."
+                    self.srv.send("Invalid volume setting.\n")
+                  
+            # Big string. First sends the size in bytes. Then keeps sending BUFFSIZE bytes per loop
+            elif parts[0] == "list":
+                xml = self._getTrackList()
+
+                # Send size
+                self.srv.send(str(len(xml))+"\n")
+
+                # Send xml list
+                ptr = 0;
+                while(1):
+                  end = ptr+BUFFSIZE
+                  if end > len(xml):
+                    end = len(xml)-1
+                  chunk = xml[ptr:(ptr+BUFFSIZE)]
+                  self.srv.send(chunk)
+                  if end == len(xml)-1:
+                    #Send EOL
+                    self.srv.send("\n")
+                    break
+                  else:
+                    ptr = ptr + BUFFSIZE
+
+            else:
+                self.srv.send("Unrecognized input.\n")
+
+          except Exception, e:
+            print "Error while running rhythmbox api functions", str(e)
+            pass
+
+        except Exception, e:
+          print "Receiving failed! Assuming disconnection...", str(e)
+          break
+  
+  def _play(self):
+    self.shell.props.shell_player.play()
+
+  def _pause(self):
+    self.shell.props.shell_player.pause()
+    
+  def _stop(self):
+    self.shell.props.shell_player.stop()
+
+  def _next(self):
+    self.shell.props.shell_player.do_next()
+
+  def _prev(self):
+    self.shell.props.shell_player.do_previous()
+
+  def _getVol(self):
+    return self.shell.props.shell_player.get_volume()[1]
+
+  def _setVol(self, vol):
+    if vol > 1.0:
+      vol = 1.0
+    elif vol < 0.0:
+      vol = 0.0
+    self.shell.props.shell_player.set_volume(vol)
+
+  def _goto(self, trackuri):
+    #Get an entry by location, use this location to play  rb_shell_player_play_entry
+    #entry = self.shell.props.db.entry_lookup_by_location("file:///mnt/sda5/Media/Audio/And%20You%20Will%20Know%20Us%20By%20The%20Trail%20Of%20Dead%20-%20And%20You%20Will%20Know%20Us%20By%20The%20Trail%20Of%20Dead/01%20-%20richter%20scale%20madness.mp3")
+    if trackuri:
+      # Find entry by uri
+      entry = self.shell.props.db.entry_lookup_by_location(trackuri)
+
+      # Source = main library (all local songs)
+      source =  self.shell.props.library_source
+
+      print "Entry title: ",entry.get_string(RB.RhythmDBPropType.TITLE)
+      self.shell.props.shell_player.play_entry(entry, source)
+
+  def _getTrackList(self):
+
+    xml = "<xml version='1.0' encoding='utf-8'><tracks>"
+
+    #Query for what user sees on the UI. That is, ordered and filtered by the user
+    #for row in self.shell.props.library_source.props.query_model:
+
+    #Query for all entries in database, regardless of what user sees on the UI
+    for row in self.shell.props.library_source.props.base_query_model:
+      entry = row[0]
+
+      #artist = base64.b64encode(unicode(entry.get_string(RB.RhythmDBPropType.ARTIST), errors='replace'))
+      #title= base64.b64encode(unicode(entry.get_string(RB.RhythmDBPropType.TITLE), errors='ignore'))
+      #album = base64.b64encode(unicode(entry.get_string(RB.RhythmDBPropType.ALBUM), errors='ignore'))
+      #url = base64.b64encode(unicode(entry.get_string(RB.RhythmDBPropType.LOCATION), errors='ignore'))
+      artist = base64.b64encode(entry.get_string(RB.RhythmDBPropType.ARTIST))
+      title= base64.b64encode(entry.get_string(RB.RhythmDBPropType.TITLE))
+      album = base64.b64encode(entry.get_string(RB.RhythmDBPropType.ALBUM))
+      url = base64.b64encode(entry.get_string(RB.RhythmDBPropType.LOCATION))
+
+      xml += "<track>"
+      xml += "<artist>"+artist+"</artist>"
+      xml += "<title>"+title+"</title>"
+      xml += "<album>"+album+"</album>"
+      xml += "<url>"+url+"</url>"
+      xml += "</track>"
+
+    xml += "</tracks></xml>"
+
+    print "Total length of XML: "+str(len(xml))
+
+    #f = open('/tmp/xml.test', 'w')
+    #f.write(xml)
+    #f.close()
+
+    return xml
